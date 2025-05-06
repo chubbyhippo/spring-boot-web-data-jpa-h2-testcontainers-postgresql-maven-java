cf1f59ad6c894a275a44a7f14ea021e4d5db5459
chubbyhippo <mkmsacc@outlook.com>
2025-05-06 21:43:30 +0700

    Initialize a basic Spring Boot project with Testcontainers
    Added a Spring Boot project setup with Maven, including configurations for Testcontainers. Included dependencies for JPA, PostgreSQL, H2, and necessary test dependencies. Configured application properties, Maven wrapper, and Git settings.

---
+++ src/test/java/io/github/chubbyhippo/demo/TestDemoApplication.java
@@ -0,0 +1,11 @@
+package io.github.chubbyhippo.demo;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(DemoApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
